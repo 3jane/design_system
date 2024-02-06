c8ddb18a9ba8432d15199bfa1082475bd0273c59
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2024-02-06 10:10:24 +0000

    feat: load all fonts during initialization

--- packages/figma/swatch_generator/src/plugin/code.ts
+++ packages/figma/swatch_generator/src/plugin/code.ts
@@ -3,18 +3,39 @@ import { initializeNetwork } from "@common/network/init";
 import { NetworkSide } from "@common/network/sides";
 import { NetworkMessages } from "@common/network/messages";
 
+import tokens from "../../../../../build/json/tokens.json";
+import { cssFontToFigma } from "@common/utils";
+
+async function loadAllFonts() {
+  const fonts = Object.values(tokens.composites.font).reduce((set, { lg, md, sm }) => {
+    set.add(lg);
+    set.add(md);
+    set.add(sm);
+
+    return set;
+  }, new Set<string>());
+
+  await Promise.allSettled(
+    [...fonts].map((it) => {
+      const { family, style } = cssFontToFigma(it);
+
+      return figma.loadFontAsync({ family, style });
+    })
+  );
+}
+
 async function bootstrap() {
-  initializeNetwork(NetworkSide.PLUGIN)
+  initializeNetwork(NetworkSide.PLUGIN);
 
   if (figma.editorType === "figma") {
     figma.showUI(__html__, {
       width: 320,
-      height: 480,
+      height: 380,
       title: "My Figma Plugin!",
     });
   }
 
-  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+  await loadAllFonts();
 
   console.log("Bootstrapped @", Networker.Side.current.getName());
 
