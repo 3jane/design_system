4218d6c5e96e77384e76ec03847f0294f5ffe292
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2024-02-22 22:07:25 +0000

    chore: change UI of plugin

--- packages/figma/swatch_generator/src/ui/App.tsx
+++ packages/figma/swatch_generator/src/ui/App.tsx
@@ -6,33 +6,39 @@ import styles from "./main.module.css";
 import { ComponentType } from "../generators/createComponent/base";
 
 function App() {
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const { type } = Object.fromEntries(new FormData(e.currentTarget));
+
+    NetworkMessages.CREATE_SWATCH.send({
+      componentType: type as ComponentType,
+    });
+  };
+
+  const handleGetId = () => {
+    NetworkMessages.TEST.send({});
+  };
+
   return (
-    <div className={styles.container}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <h2 className={styles.title}> 3DS Swatch Generator </h2>
-      <Button
-        className={styles.button}
-        onClick={() => {
-          return NetworkMessages.CREATE_SWATCH.send({
-            componentType: ComponentType.Button,
-          });
-        }}
-      >
-        Generate button swatch
+
+      <label>
+        <span>Select component</span>
+        <br />
+        <select name="type">
+          <option value={ComponentType.Icon}>Icon</option>
+          <option value={ComponentType.Button}>Button</option>
+        </select>
+      </label>
+      <br />
+      <Button type="submit">Generate</Button>
+      <br />
+      <Button type="button" onClick={handleGetId}>
+        Get id
       </Button>
-      <Button
-        className={styles.button}
-        onClick={() => {
-          return NetworkMessages.CREATE_SWATCH.send({
-            componentType: ComponentType.Input,
-          });
-        }}
-      >
-        Generate input swatch
-      </Button>
-      <Button className={styles.button} onClick={() => NetworkMessages.TEST.send({})}>
-        Test
-      </Button>
-    </div>
+    </form>
   );
 }
 
